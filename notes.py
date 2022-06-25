8b2d5293704b013b2241a679dd7d68dca6be7e45
HopeMashal <mashal.hope@gmail.com>
2022-06-25 13:07:31 +0300

    first commit

---
+++ notes.py
@@ -0,0 +1,11 @@
+""" To print something in terminal you can use {print("...")} """
+print("Hi Hope, please draw triangle")
+
+print("     /|")
+print("    / |")
+print("   /  |")
+print("  /   |")
+print(" /    |")
+print("/_____|")
+
+""" Variables """
