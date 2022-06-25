0154009b5c3fd1625014e9d3c429a535b1f85448
HopeMashal <mashal.hope@gmail.com>
2022-06-25 23:30:35 +0300

    add functions note

--- notes.py
+++ notes.py
@@ -81,7 +81,7 @@ print(name.replace("al","ro"))  #* The output is Amro
 #!""" Numbers """
 
 """ to convert the number to string use { str(number) } --> Because you can't print number with string!!"""
-print("I'm " + str(24) + " years old.")
+print("I'm " + str(24) + " years old.")   #* The output is I'm 24 years old.
 
 """ to take tha absolute value use { abs(number) } """
 print(abs(-100.2))   #* The output is 100.2
@@ -214,8 +214,32 @@ coordinates = (34,45)
 print(coordinates)  #* The output is (34, 45)
 print(coordinates[1])  #* The output is 45
 array_of_tuples = [(12,23),(22,32)]
+""" You can't do this operation --> array_of_tuples[0][0]=10 (ERROR!!)"""
 
 """ _______________________________ """
 #!""" Functions """
+
+""" To create function use { def function_name(): } """
+def say_hi():
+  print("Hi There!!")
+  print("Enjoy")
+
+say_hi()   #* The output is Hi There!!  \n  Enjoy
+
+""" Function with parameter  """
+def print_name(name):
+  print("Hello " + name)
+
+print_name("Hope!!!")   #* The output is Hello Hope!!!
+
+""" Function cube """
+def cube(num):
+  return pow(num,3)
+
+print(cube(3))  #* The output is 27.0
+
+""" _______________________________ """
+#!""" Conditionals """
+
 
 """ _______________________________ """
