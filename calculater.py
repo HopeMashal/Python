43d788ab815f48967f5e13b4debb0b77049ddee5
HopeMashal <mashal.hope@gmail.com>
2022-06-25 17:20:05 +0300

    edit calculater

--- calculater.py
+++ calculater.py
@@ -1,9 +1,12 @@
+""" In this app, I used switch and if/else statement """
 """ Ask the user to enter the numbers and the operation """
 
 Number_1 = input("Enter the first number: ")
 Number_2 = input("Enter the second number: ")
 print("Enter 1 for sum (+) \t Enter 2 for sub (-) \t Enter 3 for div (/) \t Enter 4 for mult (*) \t Enter 5 for remainder of the div (%)")
 operation = input("Enter the operation: ")
+
+""" Def. the functions """
 
 def sum_op():
     return float(Number_1)+float(Number_2)
@@ -18,6 +21,8 @@ def res_op():
 def default():
     return "Incorrect operation, please enter (1 -> + , 2 -> - , 3 -> / , 4 -> * , 5 -> %)"
 
+""" Def. the switch value """
+
 switcher = {
     1: sum_op,
     2: sub_op,
@@ -26,8 +31,12 @@ switcher = {
     5: res_op,
     }
 
+""" Return the result of the switch """ 
+
 def switch(operationResult):
     return switcher.get(operationResult, default)()
+
+""" Print the equation and the result """ 
 
 op_select = ""
 op_value = int(operation)
