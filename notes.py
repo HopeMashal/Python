9305baa41144598a5ca242e52dae73e94cb9bbb5
HopeMashal <mashal.hope@gmail.com>
2022-06-25 14:24:16 +0300

    add notes

--- notes.py
+++ notes.py
@@ -47,4 +47,17 @@ print(name.islower())
 """ to check if the words is upper case use { name_variable.isupper() } """
 print(name.isupper())
 
+""" to print the length of the sentence use { len(name_variable) } """
+print(len(name))
+
+""" to print letter from words use { name_variable[number_letter] } """
+print(name[1],name[3])
+
+""" (to know the place of the letter in the words --> number) & (to check if this letter is in the words --> Error (we can't find the letter)) use { name_variable.index("letter")} """
+print(name.index("m"))
+print(name.index("al"))
+
+""" to replace some letters use { name_variable.replace("value_to_change","new_value") } """
+print(name.replace("al","ro"))
+
 """ _______________________________ """
