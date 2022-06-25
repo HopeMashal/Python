b6bed9c3ed6c5d17f3c4fd21c9e33519367bb665
HopeMashal <mashal.hope@gmail.com>
2022-06-25 19:11:10 +0300

    add array note

--- notes.py
+++ notes.py
@@ -1,7 +1,7 @@
 """ Python Notes!! """
 
 """ You can use { #... } for inline comments
-And { """  """ } for multiline comments """
+And { """    """ } for multiline comments """
 # inline comments
 """ 
 multiline comments
@@ -146,5 +146,66 @@ print(my_data[-1])   #* The output is --> NODE
 
 """ You can change the value of any items in the array """
 my_data[0] = "HTML3" 
+
+""" Add the second array to first array use { array1.extend(array2) OR array1 += array2 OR array1 = array1 + array2} """
+array1 = ["Hope" , "Amal"]
+array2 = ["Yuki" , "Akira"]
+array1.extend(array2)
+print(array1)   #* The output is --> ['Hope', 'Amal', 'Yuki', 'Akira']
+
+""" Add new item to the array (last item) use { array.append(item_value) } """
+array2.append("Yuyu")
+print(array2)   #* The output is --> ['Yuki', 'Akira', 'Yuyu']
+
+""" Add new item to the array (any place) use { array.insert(place , item_value) } """
+array2.insert(1,"Moshy")
+print(array2)   #* The output is --> ['Yuki', 'Moshy', 'Akira', 'Yuyu']
+
+""" Remove any item from array use { array.remove(item_value) } """
+array2.remove("Yuyu")
+print(array2)  #* The output is --> ['Yuki', 'Moshy', 'Akira']
+
+""" Remove last item from array use { array.pop() } """
+last_item = array2.pop()
+print(last_item)   #* The output is --> Akira
+print(array2)  #* The output is --> ['Yuki', 'Moshy']
+
+""" Clear the array use {array.clear()} """
+array2.clear()  
+print(array2)   #* The output is --> []
+
+""" (to know the place of the item in the array --> number) & (to check if this item is in the array --> Error (we can't find the item)) use { array.index("item_value")} """
+print(array1.index("Amal"))  #* The output is 1
+
+""" Count the item in the array use { array.count(item_value) } """
+arr = ["hope","amal","hope","yuki"]
+print(arr.count("hope"))   #* The output is 2
+
+""" Sorting (a -> z) OR ( -infinite -> infinite ) use { array.sort() } """
+arr1 = ["hope","amal","yuki","mashal"]
+arr1.sort()
+print(arr1)   #* The output is ['amal', 'hope', 'mashal', 'yuki']
+
+arr2 = [55,3,4,65]
+arr2.sort()
+print(arr2)   #* The output is [3, 4, 55, 65]
+
+""" Shallow copy the array use { array.copy() } """
+old_arr = ["Hi","Hello"]
+new_arr = old_arr.copy()
+old_arr.append("There!")
+print(new_arr)   #* The output is ['Hi', 'Hello']
+print(old_arr)   #* The output is ['Hi', 'Hello', 'There!']
+
+""" Copy the array use { new_array = old_array } """
+old_arr1 = ["Hi!!","Hello!!"]
+new_arr1 = old_arr1
+old_arr1.append("There!!!")
+print(new_arr1)   #* The output is ['Hi!!', 'Hello!!', 'There!!!']
+print(old_arr1)   #* The output is ['Hi!!', 'Hello!!', 'There!!!']
+
+""" _______________________________ """
+#!""" Tuples """
+
 
 """ _______________________________ """
