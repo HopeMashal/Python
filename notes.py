8cd58de16686104c45d315a7865c6c78abc1719f
HopeMashal <mashal.hope@gmail.com>
2022-06-25 23:11:11 +0300

    add tuples notes

--- notes.py
+++ notes.py
@@ -207,5 +207,15 @@ print(old_arr1)   #* The output is ['Hi!!', 'Hello!!', 'There!!!']
 """ _______________________________ """
 #!""" Tuples """
 
+""" You can't change the tuples item value """
+""" To def. the tuples use { tuples_name = (..,..,...etc) } """
+coordinates = (34,45)
+""" You can't do this operation --> coordinates[0]=20 (ERROR!!)"""
+print(coordinates)  #* The output is (34, 45)
+print(coordinates[1])  #* The output is 45
+array_of_tuples = [(12,23),(22,32)]
+
+""" _______________________________ """
+#!""" Functions """
 
 """ _______________________________ """
