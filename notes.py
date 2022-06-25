14e0669080c740fb2a867741377ea40280a7ba8c
HopeMashal <mashal.hope@gmail.com>
2022-06-25 13:56:47 +0300

    edit the notes file

--- notes.py
+++ notes.py
@@ -9,18 +9,18 @@ print("  /   |")
 print(" /    |")
 print("/_____|")
 
-
+""" _______________________________ """
 """ Variables """
 
 name = "Hope"  
-""" You can change variable value, name="..." the variable value is string """  
+""" You can change variable value, name="..." the variable value type is string """  
 
 age = "25"
 year = 1997
-""" year=# the variable value is integer (number) """
+""" year=# the variable value type is integer (number) """
 
 average = 81.8
-""" average=# the variable value is float (number) """
+""" average=# the variable value type is float (number) """
 
 print("Hi there!")
 print("My name is " + name +". I'm " + age + " years old.")
@@ -29,3 +29,22 @@ print("I changed my name to " + name)
 
 is_Female = True
 """ Boolean input --> True or False """
+
+""" to print new line use \n """
+""" to print " use \" """
+""" to print ' use \' """
+""" to print tab use \t """
+
+""" to print the words using lower case use { name_variable.lower() } """
+print(name.lower())
+
+""" to print the words using upper case use { name_variable.upper() } """
+print(name.upper())
+
+""" to check if the words is lower case use { name_variable.islower() } """
+print(name.islower())
+
+""" to check if the words is upper case use { name_variable.isupper() } """
+print(name.isupper())
+
+""" _______________________________ """
