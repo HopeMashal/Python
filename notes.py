cd8f099e4b9d739dfe90ef9c93d5d8f714e35514
HopeMashal <mashal.hope@gmail.com>
2022-06-26 21:39:02 +0300

    add object notes

--- notes.py
+++ notes.py
@@ -295,7 +295,33 @@ print(match_string("Amal","Hope"))    #* The output is The strings don't match
 """
 
 """ _______________________________ """
-#!""" Dictionaries """
+#!""" Dictionaries (OBJECTS) """
+
+""" OBJECT use { 
+  object_name={
+    "KEY":"VAlUE"
+  }
+  KEY type number or string
+  Value type number or string or boolean or array or object
+} """
+convert_month = {
+  "Jan":"January",
+  "Feb":"February",
+  "Mar":"March",
+  "Apr":"April",
+  "May":"May",
+  "Jun":"June",
+}
+
+print(convert_month["Mar"])   #* The output is March
+print(convert_month.get("Jun"))   #* The output is June
+print(convert_month.get("Hope"))   #* The output is None
+""" print(object_name.get(KEY_value,ERROR_MSG)) """
+print(convert_month.get("Hope","The value doesn't exist"))   #* The output is The value doesn't exist 
+print(convert_month.get("Jan","The value doesn't exist"))   #* The output is January
+
+""" _______________________________ """
+#!""" WHILE LOOP """
 
 
 """ _______________________________ """
