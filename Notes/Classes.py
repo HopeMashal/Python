7568553b6dff7a946a43d8c0dd451d634e74e403
HopeMashal <mashal.hope@gmail.com>
2022-06-27 18:25:38 +0300

    edit class note

--- Notes/Classes.py
+++ Notes/Classes.py
@@ -5,8 +5,8 @@
 
 from Employee import Employee  #? from file_name import class_name
 
-employee1 = Employee("Hope",24,"Meta", False)
-employee2 = Employee("Yuki",40,"Whatsapp", True)
+employee1 = Employee("Hope",24,"Meta", False, 3.5, 1000)
+employee2 = Employee("Yuki",60,"Whatsapp", True, 5, 1500)
 
 print("The first employee is",employee1.name,". She is",employee1.age,"years old.")
 print("The second employee is",employee2.name,". She is",employee2.age,"years old.")
@@ -16,10 +16,23 @@ print("The second employee is",employee2.name,". She is",employee2.age,"years ol
 
 #* Class Functions
 
+print("Is the employee",employee1.name,"excellent?",employee1.is_excellent())
+print("Is the employee",employee2.name,"excellent?",employee2.is_excellent())
+""" #* The output is 
+    Is the employee Hope excellent? False
+    Is the employee Yuki excellent? True
+"""
+
+employee1.bonus()
+employee2.bonus()
+""" #* The output is 
+    Dear Hope  your salary is still 1000 (NO BONUS ADDED!)
+    Dear Yuki your salary increased to 2000 (500 BONUS ADDED!)
+"""
 
 """ _______________________________ """
 
-#* Class Inheritance
+#* Class Inheritance (الوراثة)
 
 
 
--- Notes/Employee.py
+++ Notes/Employee.py
@@ -2,10 +2,25 @@
 #!""" Classes Case """
 
 class Employee:
-  def __init__(self, name, age, department, is_manager):
+  def __init__(self, name, age, department, is_manager, rating, salary):
     self.name = name
     self.age = age
     self.department = department 
     self.is_manager = is_manager
+    self.rating = rating  # rating from 0 to 5
+    self.salary = salary
+
+  def is_excellent(self):
+    if self.rating >= 4.5:
+      return True
+    else:
+      return False
+
+  def bonus(self):
+    if self.age == 60:
+      self.salary += 500
+      print("Dear",self.name,"your salary increased to",self.salary,"(500 BONUS ADDED!)")
+    else:
+      print("Dear",self.name," your salary is still",self.salary,"(NO BONUS ADDED!)")
 
 """ _______________________________ """
