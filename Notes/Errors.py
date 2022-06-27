02584084e6b48e7e3913701c942ae495fcdb06fc
HopeMashal <mashal.hope@gmail.com>
2022-06-27 14:45:49 +0300

    add error notes

--- Notes/Errors.py
+++ Notes/Errors.py
@@ -1,5 +1,23 @@
 """ _______________________________ """
 #!""" Errors """
 
+""" Send error msg when user enter string """
+try:
+  value = int(input("Enter a number: "))
+  print(value)
+except:   #* In general
+  print("ERROR: Invalid Input")
+print("SUCCESS")
+
+""" Send error msg (number divide by zero) or (string)"""
+try:
+  value = int(input("Enter a number: "))
+  print(value)
+  reset = value/0
+except ZeroDivisionError:  #* Case (TYPE) of error
+  print("ERROR: YOU CAN'T DIVIDE BY ZERO!!!")
+except ValueError as error:
+  print("ERROR: ",error)
+print("SUCCESS")
 
 """ _______________________________ """
