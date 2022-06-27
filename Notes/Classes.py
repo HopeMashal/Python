d7d557d474da97d5c8ceb62bbed3063f59388020
HopeMashal <mashal.hope@gmail.com>
2022-06-27 18:02:15 +0300

    edit classes note

--- Notes/Classes.py
+++ Notes/Classes.py
@@ -1,5 +1,27 @@
 """ _______________________________ """
 #!""" Classes """
 
+#* Classes & Objects
+
+from Employee import Employee  #? from file_name import class_name
+
+employee1 = Employee("Hope",24,"Meta", False)
+employee2 = Employee("Yuki",40,"Whatsapp", True)
+
+print("The first employee is",employee1.name,". She is",employee1.age,"years old.")
+print("The second employee is",employee2.name,". She is",employee2.age,"years old.")
+
+
+""" _______________________________ """
+
+#* Class Functions
+
+
+""" _______________________________ """
+
+#* Class Inheritance
+
+
+
 
 """ _______________________________ """
