5ebb0818b167213da9170bbd1d0f4eae4d9096f1
HopeMashal <mashal.hope@gmail.com>
2022-06-28 19:42:39 +0300

    add comments

--- Apps/Guessing_Game.py
+++ Apps/Guessing_Game.py
@@ -1,8 +1,8 @@
-secret_word = "Jofee"
-guess = ""
-guess_count = 0
-guess_limit = 3
-out_of_guesses = False
+secret_word = "Jofee"   #? The secret word value
+guess = ""   #? The guess value
+guess_count = 0  #? The counter value
+guess_limit = 3  #? The limit value of guess
+out_of_guesses = False  #? If guess_count = 3 => out_of_guesses = True
 
 while guess != secret_word and not out_of_guesses:
   if guess_count < guess_limit:
--- Apps/Hangman_Game.py
+++ Apps/Hangman_Game.py
@@ -1,16 +1,16 @@
 import random
 
-Words=["html","css","java","javascript","react","node","python","csharp","linux"]
+Words=["html","css","java","javascript","react","node","python","csharp","linux"]  #? The programming languages array
 
-word_select = Words[random.randint(0,len(Words))]
+word_select = Words[random.randint(0,len(Words))]  #? Select random programming language
 
-word_guess = ""
+word_guess = ""  #? The guess value is empty
 for i in range(len(word_select)):
-  word_guess = word_guess + "*"
+  word_guess = word_guess + "*"  #?The guess value is {***}
 
-guess_count = 0
-guess_limit = 6
-out_of_guesses = False
+guess_count = 0  #? The counter value
+guess_limit = 6  #? The limit value of guess
+out_of_guesses = False   #? If guess_count = 3 => out_of_guesses = True
 
 print("Welcome in Hangman Game!!")
 print("Guess the Programming Language:")
@@ -22,8 +22,8 @@ while word_guess != word_select and not out_of_guesses:
   if guess_count < guess_limit:
     guess_input = input("Enter Guess: ")
     guess = guess_input.lower()
-    for letter in range(len(word_select)):
-      if guess == word_select[letter]:
+    for letter in range(len(word_select)):  #? check if the input value is equal to one of the letters of the selected word
+      if guess == word_select[letter]:  #? change the guess value to {*x*}
         if letter == 0:
           word_guess = guess + word_guess[1:]
         elif letter > 0 and letter < len(word_guess)-1:
@@ -33,6 +33,7 @@ while word_guess != word_select and not out_of_guesses:
         print("The word is: ",word_guess)
     if guess not in word_select:
       guess_count += 1
+      print("The word is: ",word_guess)
       print("You have",(guess_limit-guess_count),"guesses")
   else:
     out_of_guesses = True
