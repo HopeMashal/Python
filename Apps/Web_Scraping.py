1a298103e5481d1723e78ed84399da48fbe942a5
HopeMashal <mashal.hope@gmail.com>
2022-06-29 14:58:18 +0300

    done -> web scraping

--- Apps/Web_Scraping.py
+++ Apps/Web_Scraping.py
@@ -15,32 +15,55 @@ skills = []
 links = []
 salary = []
 responsibilities = []
+date = []
 
-# second step use requests to fetch the url
-result = requests.get("https://wuzzuf.net/search/jobs/?q=python&a=hpb")   #Website Link
+page_num = 0
 
-# third step save page content/markup
-src = result.content
+while True:
 
-# 4th step create soup object to prase content
-soup = BeautifulSoup(src, "lxml")
+  try:
+    # second step use requests to fetch the url
+    result = requests.get(f"https://wuzzuf.net/search/jobs/?a=hpb&q=python&start={page_num}")   #Website Link
 
-# 5th step find the elements containing info we need
-# Job titles, job skills, company names, location names
-job_titles = soup.find_all("h2", {"class":"css-m604qf"})  #From website (h2 tag) --> class of (h2 tag -> job titles)
-# soup.find_all(html_tag , {features} )
+    # third step save page content/markup
+    src = result.content
 
-company_names = soup.find_all("a",{"class":"css-17s97q8"})
-location_names = soup.find_all("span",{"class":"css-5wys0k"})
-job_skills = soup.find_all("div",{"class":"css-y4udm8"})
+    # 4th step create soup object to prase content
+    soup = BeautifulSoup(src, "lxml")
 
-# 6th step loop over returned lists to extract needed info into other lists
-for i in range(len(job_titles)):
-  job_title.append(job_titles[i].text)
-  links.append("https://wuzzuf.net"+job_titles[i].find("a").attrs['href'])
-  company_name.append(company_names[i].text)
-  location_name.append(location_names[i].text)
-  skills.append(job_skills[i].text)
+    page_limit = int(soup.find("strong").text)
+    if(page_num > page_limit//15):
+      print("Pages ended, terminate!!")
+      break
+
+    # 5th step find the elements containing info we need
+    # Job titles, job skills, company names, location names
+    job_titles = soup.find_all("h2", {"class":"css-m604qf"})  #From website (h2 tag) --> class of (h2 tag -> job titles)
+    # soup.find_all(html_tag , {features} )
+
+    company_names = soup.find_all("a",{"class":"css-17s97q8"})
+    location_names = soup.find_all("span",{"class":"css-5wys0k"})
+    job_skills = soup.find_all("div",{"class":"css-y4udm8"})
+    posted_new = soup.find_all("div",{"class":"css-4c4ojb"})
+    posted_old = soup.find_all("div",{"class":"css-do6t5g"})
+    posted = [*posted_new,*posted_old]
+
+    # 6th step loop over returned lists to extract needed info into other lists
+    for i in range(len(job_titles)):
+      job_title.append(job_titles[i].text)
+      links.append("https://wuzzuf.net"+job_titles[i].find("a").attrs['href'])
+      company_name.append(company_names[i].text.replace("-","").strip())
+      location_name.append(location_names[i].text)
+      skills.append(job_skills[i].text)
+      date.append(posted[i].text.strip())
+
+    page_num += 1
+    print("Page switched!")
+
+  except:
+    print("ERROR!!")
+    break
+
 
 for link in links:
   result_link = requests.get(link)
@@ -48,7 +71,6 @@ for link in links:
   soup_link = BeautifulSoup(src_link,"lxml")
   #! I have a problem with salaries and requirement
   #salaries = soup_link.find("span",{"class":"css-4xky9y"}) #? Because it's not a unique value, there is a problem with it
-  #requirements = soup_link.find("div",{"class":"css-1t5f0fr"}).find("ul")
   #salary.append(salaries.text.strip())
   salaries = "Confidential"
   salary.append(salaries.strip())
@@ -61,8 +83,9 @@ for link in links:
   requirements = "Bachelor/Master of Science in relevant area or equivalent formal education."
   responsibilities.append(requirements)
 
+
 # 7th step create csv file and fill it with values
-file_list = [job_title,company_name,location_name,skills, links, salary, responsibilities]
+file_list = [job_title,company_name,date,location_name,skills, links, salary, responsibilities]
 exported = zip_longest(*file_list)  #* (*->UNPACKING)
 """ 
   Ex :
@@ -74,15 +97,12 @@ exported = zip_longest(*file_list)  #* (*->UNPACKING)
 
 with open("jobstest.csv","w") as myfile:
   wr = csv.writer(myfile)
-  wr.writerow(["Job Title","Company Name","Location","Skills","Job Link","Salary","Job Requirements"])
+  wr.writerow(["Job Title","Company Name","Date","Location","Skills","Job Link","Salary","Job Requirements"])
   wr.writerows(exported)
 
 # 8th step to fetch the link of the job and fetch in page details
 # Salary, job requirements
 
-
 # 9th step is to do the above for all pages
 
-
-# 10th step is to optimize code and clean data
 
