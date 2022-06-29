a3028170200af502f7957ef95b9b19cf4d581a5e
HopeMashal <mashal.hope@gmail.com>
2022-06-29 12:27:23 +0300

    edit web scraping

--- Apps/Web_Scraping.py
+++ Apps/Web_Scraping.py
@@ -12,6 +12,9 @@ job_title = []
 company_name = []
 location_name = []
 skills = []
+links = []
+salary = []
+responsibilities = []
 
 # second step use requests to fetch the url
 result = requests.get("https://wuzzuf.net/search/jobs/?q=python&a=hpb")   #Website Link
@@ -34,12 +37,32 @@ job_skills = soup.find_all("div",{"class":"css-y4udm8"})
 # 6th step loop over returned lists to extract needed info into other lists
 for i in range(len(job_titles)):
   job_title.append(job_titles[i].text)
+  links.append("https://wuzzuf.net"+job_titles[i].find("a").attrs['href'])
   company_name.append(company_names[i].text)
   location_name.append(location_names[i].text)
   skills.append(job_skills[i].text)
 
+for link in links:
+  result_link = requests.get(link)
+  src_link = result_link.content
+  soup_link = BeautifulSoup(src_link,"lxml")
+  #! I have a problem with salaries and requirement
+  #salaries = soup_link.find("span",{"class":"css-4xky9y"}) #? Because it's not a unique value, there is a problem with it
+  #requirements = soup_link.find("div",{"class":"css-1t5f0fr"}).find("ul")
+  #salary.append(salaries.text.strip())
+  salaries = "Confidential"
+  salary.append(salaries.strip())
+  #requirements= soup_link.find("div",{"class":"css-1t5f0fr"}).ul
+  #respon_text =""
+  #for li in requirements.find_all("li"):
+  #  respon_text += li.text+"| "
+  #respon_text = respon_text[:-2]
+  #responsibilities.append(respon_text)
+  requirements = "Bachelor/Master of Science in relevant area or equivalent formal education."
+  responsibilities.append(requirements)
+
 # 7th step create csv file and fill it with values
-file_list = [job_title,company_name,location_name,skills]
+file_list = [job_title,company_name,location_name,skills, links, salary, responsibilities]
 exported = zip_longest(*file_list)  #* (*->UNPACKING)
 """ 
   Ex :
@@ -51,7 +74,7 @@ exported = zip_longest(*file_list)  #* (*->UNPACKING)
 
 with open("jobstest.csv","w") as myfile:
   wr = csv.writer(myfile)
-  wr.writerow(["Job Title","Company Name","Location","Skills"])
+  wr.writerow(["Job Title","Company Name","Location","Skills","Job Link","Salary","Job Requirements"])
   wr.writerows(exported)
 
 # 8th step to fetch the link of the job and fetch in page details
