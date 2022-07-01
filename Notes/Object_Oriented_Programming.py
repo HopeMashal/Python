971589497a082a32fc6de3ca59405b44f471aef3
HopeMashal <mashal.hope@gmail.com>
2022-07-01 11:48:16 +0300

    edit oop file

--- Notes/Object_Oriented_Programming.py
+++ Notes/Object_Oriented_Programming.py
@@ -1,11 +1,30 @@
 """ _______________________________ """
 #!""" Object Oriented Programming """
 
-#? Types of Object Oriented Programming:
+#? Types of Object Oriented Programming (OOP):
 #* 1- Encapsulation
 #* 2- Polymorphism
 #* 3- Abstraction
 #* 4- Inheritance
+
+#? What's Primitive Data Types?
+#* Type that save a Single Simple Value, ex: Byte,Int,Float,Double,Char,Boolean
+
+# Struct (structure) --> Grouping variables together makes it more easier (variables of different types) ==> It's not possible to change any value inside it
+
+#! Object --> object is an instance of a class / class is a template for the object  ==> Grouping variables together & It's possible to change any value inside it
+
+""" 
+  Example :
+    Class Feel: #Feel in chess
+      Position
+      Color
+      Boolean --> True (dead), False (Not dead)
+      Move(): return newPos (the same value for all feels in chess, so I can define it inside the class)
+
+      feel1 = new Feel(3,"white", false)
+      feel2 = new Feel(-3,"black", false)
+"""
 
 
 
