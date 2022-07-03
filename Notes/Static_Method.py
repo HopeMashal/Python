f8781a2881324fba90b0d96dcfe717911533cdf7
HopeMashal <mashal.hope@gmail.com>
2022-07-03 23:41:51 +0300

    edit static method

---
+++ Notes/Static_Method.py
@@ -0,0 +1,16 @@
+class Math:
+  @staticmethod
+  def add(x,y):
+    return x+y
+
+  @staticmethod
+  def add5(num):
+    return num+5
+
+  @staticmethod
+  def add10(num):
+    return num+10
+
+  @staticmethod
+  def PI():
+    return 3.14
