2d6e37d6bfb4e9d49a90f9712b4ec4e727e511b6
HopeMashal <mashal.hope@gmail.com>
2022-07-04 00:29:44 +0300

    edit inher&poly

--- Notes/Inheritance_&_Polymorphism.py
+++ Notes/Inheritance_&_Polymorphism.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 class Person:
   def __init__(self,name,age):
     self.__name=name
@@ -5,6 +7,10 @@ class Person:
 
   def display(self):
     return f"Name is {self.__name} and age is {self.__age}"
+
+  @classmethod
+  def initFromBirthYear(cls,name,birthday,extra):
+    return cls(name, date.today().year - birthday, extra)
 
 class Man(Person):
   __gender = 'Male'
@@ -22,4 +28,26 @@ class Man(Person):
 man_1 = Man('Akira',30,'High')
 print(man_1.display()) #* The output is Name is Akira and age is 30 and voice is High and gender is Male
 print(man_1.no_of_men) #* The output is 1
+man = Man.initFromBirthYear('Amro',1998,"Hard")
+print(man.display()) #* The output is Name is Amro and age is 24 and voice is Hard and gender is Male
+print(man_1.no_of_men) #* The output is 2
 
+class Woman(Person):
+  __gender = 'Female'
+  no_of_women =0
+
+  def __init__(self, name, age,hair):
+    super().__init__(name,age)
+    self.__hair = hair
+    Woman.no_of_women +=1
+
+  def display(self):
+    string = super().display()
+    return string + f" and hair is {self.__hair} and gender is {self.__gender}"
+
+woman_1 = Woman('Yuki',20,'Curly')
+print(woman_1.display()) #* The output is Name is Yuki and age is 20 and hair is Curly and gender is Female
+print(woman_1.no_of_women) #* The output is 1
+woman = Woman.initFromBirthYear('Yuyu',1996,"Curly")
+print(woman.display()) #* The output is Name is Yuyu and age is 26 and hair is Curly and gender is Female
+print(woman_1.no_of_women) #* The output is 2
