137fe8b8231954eaefd83130913eaec682eb9dac
HopeMashal <mashal.hope@gmail.com>
2022-07-04 00:43:06 +0300

    edit abstract

--- Notes/Abstraction.py
+++ Notes/Abstraction.py
@@ -29,3 +29,8 @@ class Rect(Shape):
 
   def perimeter(self):
     return 2*(self.__width+self.__length)
+
+square = Square(10)
+print(f"Square area is {square.area()} and perimeter is {square.perimeter()}") #* The output is Square area is 100 and perimeter is 40
+rect = Rect(5,3)
+print(f"Rectangle area is {rect.area()} and perimeter is {rect.perimeter()}") #* The output is Rectangle area is 15 and perimeter is 16
