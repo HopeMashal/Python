b7d6f095fd04c9b68a807b8756e09e021c94bfda
HopeMashal <mashal.hope@gmail.com>
2022-07-04 00:36:42 +0300

    edit abstraction

---
+++ Notes/Abstraction.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+  @abstractmethod
+  def area(self):
+    pass
+
+  @abstractmethod
+  def perimeter(self):
+    pass
+
+class Square(Shape):
+  def __init__(self, side):
+    self.__side=side
+
+  def area(self):
+    
