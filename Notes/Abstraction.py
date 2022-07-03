4a506c9f8d7e6725b32bf10e042871694b2c9d62
HopeMashal <mashal.hope@gmail.com>
2022-07-04 00:40:19 +0300

    edit abstract

--- Notes/Abstraction.py
+++ Notes/Abstraction.py
@@ -14,4 +14,18 @@ class Square(Shape):
     self.__side=side
 
   def area(self):
-    
+    return self.__side *self.__side
+
+  def perimeter(self):
+    return self.__side*4
+
+class Rect(Shape):
+  def __init__(self, length, width):
+    self.__length=length
+    self.__width=width
+
+  def area(self):
+    return self.__length *self.__width
+
+  def perimeter(self):
+    return 2*(self.__width+self.__length)
