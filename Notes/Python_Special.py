124c38b2c03fedc4d8d3dc25cdad72084c456a04
HopeMashal <mashal.hope@gmail.com>
2022-07-05 18:04:12 +0300

    edit python special

--- Notes/Python_Special.py
+++ Notes/Python_Special.py
@@ -2,13 +2,13 @@
 #!""" Python Special/Dunder Methods """
 
 print("--------")
-print(1+1)
+print(1+1) #* The output is 2
 print("--------")
-print("Hope"+" Mashal")
+print("Hope"+" Mashal") #* The output is Hope Mashal
 print("--------")
-print(3*3)
+print(3*3) #* The output is 9
 print("--------")
-print("Hope"*3)
+print("Hope"*3) #* The output is HopeHopeHope
 print("--------")
 
 """ _______________________________ """
