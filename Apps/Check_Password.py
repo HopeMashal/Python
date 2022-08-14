1e54ceca77031bb6590a433baa3905a381cd9cab
HopeMashal <mashal.hope@gmail.com>
2022-08-14 09:27:22 +0300

    edit check password file

--- Apps/Check_Password.py
+++ Apps/Check_Password.py
@@ -21,37 +21,38 @@ def checkPassword(password):
       print("The password is valid!!")
 
     elif(not hasUpper and hasLower and hasNumber and hasSymbol):
-      print("The password is valid!! Please add upper case")
+      print("The password is invalid!! Please add upper case")
     elif(not hasUpper and not hasLower and hasNumber and hasSymbol):
-      print("The password is valid!! Please add upper case and lower case")
+      print("The password is invalid!! Please add upper case and lower case")
     elif(not hasUpper and hasLower and not hasNumber and hasSymbol):
-      print("The password is valid!! Please add upper case and number")
+      print("The password is invalid!! Please add upper case and number")
     elif(not hasUpper and hasLower and hasNumber and not hasSymbol):
-      print("The password is valid!! Please add upper case and symbol")
+      print("The password is invalid!! Please add upper case and symbol")
     elif(not hasUpper and not hasLower and not hasNumber and hasSymbol):
-      print("The password is valid!! Please add upper case and lower case and number")
+      print("The password is invalid!! Please add upper case and lower case and number")
     elif(not hasUpper and not hasLower and hasNumber and not hasSymbol):
-      print("The password is valid!! Please add upper case and lower case and symbol")
+      print("The password is invalid!! Please add upper case and lower case and symbol")
     elif(not hasUpper and hasLower and not hasNumber and not hasSymbol):
-      print("The password is valid!! Please add upper case and number and symbol")
+      print("The password is invalid!! Please add upper case and number and symbol")
 
     elif( hasUpper and not hasLower and hasNumber and hasSymbol):
-      print("The password is valid!! Please add lower case")
+      print("The password is invalid!! Please add lower case")
     elif( hasUpper and not hasLower and not hasNumber and hasSymbol):
-      print("The password is valid!! Please add lower case and number")
+      print("The password is invalid!! Please add lower case and number")
     elif( hasUpper and not hasLower and hasNumber and not hasSymbol):
-      print("The password is valid!! Please add lower case and symbol")
+      print("The password is invalid!! Please add lower case and symbol")
     elif( hasUpper and not hasLower and not hasNumber and not hasSymbol):
-      print("The password is valid!! Please add lower case and number and symbol")
+      print("The password is invalid!! Please add lower case and number and symbol")
 
     elif( hasUpper and hasLower and not hasNumber and hasSymbol):
-      print("The password is valid!! Please add number and number")
+      print("The password is invalid!! Please add number")
     elif( hasUpper and hasLower and not hasNumber and not hasSymbol):
-      print("The password is valid!! Please add number and symbol")
+      print("The password is invalid!! Please add number and symbol")
 
     elif( hasUpper and hasLower and hasNumber and not hasSymbol):
-      print("The password is valid!! Please add symbol")
+      print("The password is invalid!! Please add symbol")
   else:
     print("The password is invalid!! Password must be at least 8, and less than 20")
 
-checkPassword("dsadFSDA")
+myPassword = input("Please Enter your password: ")
+checkPassword(myPassword)
